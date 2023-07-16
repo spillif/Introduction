5dbeed2fff1a2eb9c21a99f4aecd6c1a31a58696
Fillips <fillipsbui@gmail.com>
2023-07-16 20:39:24 +0700

    Create data.r

---
+++ data.r
@@ -0,0 +1,24 @@
+certifications <- tribble(
+    ~area, ~accomplishment, ~year, ~where, ~detail,
+    "Certified Lean Six Sigma Green Belt", NA, "", "", NA,
+    "Certified Lean Six Sigma White Belt", NA, "", "", NA,
+    "Microsoft Software", "Various Applications", "", "", "Microsoft Excel - 112",
+    "Microsoft Software", "Various Applications", "", "", "Microsoft Word - 22",
+    "Microsoft Software", "Various Applications", "", "", "Microsoft Outlook - 21",
+    "Microsoft Software", "Various Applications", "", "", "Microsoft Power Point - 18",
+    "Microsoft Software", "Various Applications", "", "", "Share Point - 9",
+    "Microsoft Software", "Various Applications", "", "", "Microsoft Project - 6",
+    "Microsoft Software", "Various Applications", "", "", "Microsoft Access - 3",
+    "Microsoft Software", "Various Applications", "", "", "Microsoft Power BI - 3"
+)
+
+skills <- tribble(
+    ~area, ~skills,
+    "Programming Languages", "BASH, Batch, CCL, DAX, JavaScript, Python, R, T-SQL, PL-SQL, VBA",
+    "Markup Languages", "CSS, HTML, LaTeX, Markdown, RMarkdown, SASS, Groff/Troff",
+    "Data Interchange Formats", "CSV, JSON, XML, Feather",
+    "Other Languages", "YAML",
+    "Version Control", "Git",
+    "Text Editors", "Discern Visual Developer, RStudio, SQL Server Management Studio, VIM, Visual Studio, Visual Studio Code",
+    "Microsoft Office", "Excel, Outlook, OneNote, PowerBI, PowerPoint, Visio, Word"
+)
