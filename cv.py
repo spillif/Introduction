b69a920e2bb63c023a8dfafd9d3efc056bdb93c9
Fillips <fillipsbui@gmail.com>
2023-08-15 18:32:04 +0700

    initial commit

--- cv.py
+++ cv.py
@@ -5,10 +5,9 @@ from PIL import Image
 
 
 # --- PATH SETTINGS ---
-current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
-css_file = current_dir / "styles" / "main.css"
-resume_file = current_dir / "assets" / "CV.pdf"
-profile_pic = current_dir / "assets" / "me.jpg"
+css_file = "https://github.com/spillif/Introduction/blob/main/style/main.css"
+resume_file = "https://github.com/spillif/Introduction/blob/main/assets/CV.pdf"
+profile_pic = "https://github.com/spillif/Introduction/blob/main/assets/me.jpg"
 
 # --- GENERAL SETTINGS ---
 PAGE_TITLE = "Digital CV | Quy Phuc (Fillips) Bui"
